f324fbb012d6639fbc3784609466d5e8679fa2cb
bizsam <rampalli1@gmail.com>
2024-03-11 12:44:41 +0530

    first

---
+++ veronika/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Theme Name: Osclass Veronika Premium Theme
+Theme URI: https://osclasspoint.com/osclass-themes/general/veronika-osclass-theme_i63
+Description: Easy flat responsive osclass theme for any device with unique design and powerful functions.
+Version: 1.6.3
+Author: MB Themes
+Author URI: https://osclasspoint.com/
+Widgets: header,footer
+Theme update URI: veronika-theme-full-responsive
+Product Key: mqiokDukjuGMkrZ1taSX
+*/
+?>
